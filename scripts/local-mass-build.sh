815279b2e0846e0ce91552b7b38b4ecd78a51c4d
C.Lee Taylor <leet@leenx.org>
2025-09-05 08:31:42 +0200

    add riscv64gc-unknown-linux-gnu for local full build script

--- scripts/local-mass-build.sh
+++ scripts/local-mass-build.sh
@@ -11,6 +11,7 @@ export HTTP_PROXY=http://192.168.101.10:3142
 
 cross build ${build_opts} --target aarch64-unknown-linux-gnu
 cross build ${build_opts} --target x86_64-unknown-linux-gnu
+cross build ${build_opts} --target riscv64gc-unknown-linux-gnu
 
 cargo build ${build_opts} --target x86_64-apple-darwin
 cargo build ${build_opts} --target aarch64-apple-darwin
